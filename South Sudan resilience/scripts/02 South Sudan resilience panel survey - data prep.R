a7f30c835375dd460600f729a4c4a7921c777002
Dan Killian <dkillian@gmail.com>
2023-05-29 22:42:41 -0400

    djk

--- South Sudan resilience/scripts/02 South Sudan resilience panel survey - data prep.R
+++ South Sudan resilience/scripts/02 South Sudan resilience panel survey - data prep.R
@@ -3,7 +3,7 @@
 
 #source("scripts/0 South Sudan resilience panel survey - prep.r")
 
-d <- read_csv("data/daily/mels_resilience_panel_survey_english (5-26-23).csv")
+d <- read_csv("data/daily/mels_resilience_panel_survey_english (5-29-23).csv")
 
 d <- d %>%
   mutate(id = 1:nrow(d)) %>%
