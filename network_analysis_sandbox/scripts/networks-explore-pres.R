4fe3df0fcaa7c05ec0dd4b304f8a4bc0a64fb084
Calhoon <BRIAN.CALHOON@tetratech.com>
2023-09-13 11:29:48 -0400

    BC

--- network_analysis_sandbox/scripts/networks-explore-pres.R
+++ network_analysis_sandbox/scripts/networks-explore-pres.R
@@ -11,20 +11,20 @@ df <- df |>
 g <- graph_from_data_frame(df, directed = TRUE)
 
 
-g_plot <- ggraph(g, layout = "kk") +
-  geom_edge_link(size = 4
+g_plot <- ggraph(g)+#, layout = "kk") +
+  geom_edge_link(width = 1
                  , color = "#6639B7") +
   geom_node_point(color = "white"
-                  , size = 35)+
+                  , size = 24)+
   geom_node_text(aes(label = name)
                  , color = "#6639B7"
-                 , size = 8)+ 
-  
-  theme_void()
+                 , size = 4)+ 
+  theme_graph(plot_margin = unit(c(1, 0, 1, 0), "cm"))
 
+g_plot
 ggsave(plot = g_plot
        , filename  = "friend-plot.png"
        , path = "./network_analysis_sandbox/viz/"
        , height = 4
-       , width = 6
+       , width = 5
        , units = "in")
