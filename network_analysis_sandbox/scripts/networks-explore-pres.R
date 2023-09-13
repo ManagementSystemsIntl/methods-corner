27676ea6f9fe5c1db3c289cbd47550e3d61f1bb5
Calhoon <BRIAN.CALHOON@tetratech.com>
2023-09-13 14:41:32 -0400

    BC

--- network_analysis_sandbox/scripts/networks-explore-pres.R
+++ network_analysis_sandbox/scripts/networks-explore-pres.R
@@ -1,4 +1,7 @@
+source(here::here("./network_analysis_sandbox/prep/prep.R"))
 
+#set seed
+set.seed(1374)
 
 #develop random network
 
@@ -11,14 +14,14 @@ df <- df |>
 g <- graph_from_data_frame(df, directed = TRUE)
 
 
-g_plot <- ggraph(g, layout = "stress") +
+g_plot <- ggraph(g, layout = "kk") +
   geom_edge_link(width = 1
                  , color = "#6639B7") +
   geom_node_point(color = "white"
-                  , size = 20)+
+                  , size = 16)+
   geom_node_text(aes(label = name)
                  , color = "#6639B7"
-                 , size = 4)+ 
+                 , size = 2)+ 
   theme_graph(plot_margin = unit(c(1, 0, 1, 0), "cm"))
 
 g_plot
@@ -26,5 +29,5 @@ ggsave(plot = g_plot
        , filename  = "friend-plot.png"
        , path = "./network_analysis_sandbox/viz/"
        , height = 4
-       , width = 5
+       , width = 6
        , units = "in")
