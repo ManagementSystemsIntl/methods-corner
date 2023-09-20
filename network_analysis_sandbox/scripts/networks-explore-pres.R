e32092883173d1102b9e771d33db92117e829774
Calhoon <BRIAN.CALHOON@tetratech.com>
2023-09-20 17:35:15 -0400

    BC

--- network_analysis_sandbox/scripts/networks-explore-pres.R
+++ network_analysis_sandbox/scripts/networks-explore-pres.R
@@ -110,6 +110,104 @@ ggsave(plot = g_ment
        , width = 7
        , units = "in")
 
+#Ego graph from person 1
+#make the ego graph
+ego <- make_ego_graph(graph_ment
+                      , diameter(graph_ment)
+                      , nodes = 1
+                      , mode = c("all"))[[1]]
+
+#create a vector of distances from person 1
+dists <- distances(ego, "1")
+
+#add color to the graph object
+V(ego)$color <- my_pal[dists+1]
+
+#Ego graph from person 6
+#make the ego graph
+ego6 <- make_ego_graph(graph_ment
+                      , diameter(graph_ment)
+                      , nodes = 6
+                      , mode = c("all"))[[1]]
+
+#create a vector of distances from person 6
+dists1 <- distances(ego, "6")
+
+#add color to the graph object
+V(ego6)$color <- my_pal[dists1+1]
+
+
+# Visualize the network based on geodesic distance from vertex 184 (patient zero).
+png("./network_analysis_sandbox/viz/distances.png"
+    , width=8, height=6, units="in", res=300)
+par(mfrow = c(1,2)
+    , mar = (c(1,1,1,1)))
+plot(ego, 
+     vertex.label = dists, 
+     vertex.label.color = "white",
+     vertex.label.cex = 1.5,
+     edge.color = 'black',
+     vertex.size = 16,
+     main = "Geodesic Distances from Person 1"
+)
+plot(ego6, 
+     vertex.label = dists1, 
+     vertex.label.color = "white",
+     vertex.label.cex = 1.5,
+     edge.color = 'black',
+     vertex.size = 16,
+     main = "Geodesic Distances from Person 6"
+)
+dev.off()
+
+#identify the largest cliques in the mentor graph
+largest_cliques(graph_ment)
+
+# Determine all maximal cliques in the network and assign to object 'clq'
+clq <- max_cliques(graph_ment)
+
+# Calculate the size of each maximal clique.
+table(unlist(lapply(clq, length)))
+
+# Assign largest cliques output to object 'lc'
+lc <- largest_cliques(graph_ment)
+
+# Create two new undirected subgraphs, each containing only the vertices of each largest clique.
+gs1 <- as.undirected(subgraph(graph_ment, lc[[1]]))
+gs2 <- as.undirected(subgraph(graph_ment, lc[[2]]))
+gs3 <- as.undirected(subgraph(graph_ment, lc[[3]]))
+
+# Plot the two largest cliques side-by-side
+
+par(mfrow=c(1,3)) # To plot two plots side-by-side
+
+plot(gs1,
+     vertex.label.color = "black", 
+     vertex.label.cex = 0.9,
+     vertex.size = 0,
+     edge.color = 'gray28',
+     main = "Largest Clique 1",
+     layout = layout.circle(gs1)
+)
+
+plot(gs2,
+     vertex.label.color = "black", 
+     vertex.label.cex = 0.9,
+     vertex.size = 0,
+     edge.color = 'gray28',
+     main = "Largest Clique 2",
+     layout = layout.circle(gs2)
+)
+plot(gs3,
+     vertex.label.color = "black", 
+     vertex.label.cex = 0.9,
+     vertex.size = 0,
+     edge.color = 'gray28',
+     main = "Largest Clique 3",
+     layout = layout.circle(gs1)
+)
+dev.off()
+
 #testing out community finding algorithm
 fun <- fastgreedy.community(graph_ment)
 
@@ -117,12 +215,17 @@ fun2 <- edge.betweenness.community(graph_ment)
 
 fun3 <- cluster_fast_greedy(graph_ment)
 
-png("./network_analysis_sandbox/viz/community.png", width=8, height=6, units="in", res=300)
+fun4 <- infomap.community(graph_ment)
+
+#plot all the community network charts together
+png("./network_analysis_sandbox/viz/community.png"
+    , width=8, height=6, units="in", res=300)
 par(mfrow = c(2, 2)
     , mar = c(1, 1, 1, 1))
 plot(fun, graph_ment, main = "Communities Option 1")
 plot(fun2, graph_ment, main = "Communities Option 2")
 plot(fun3, graph_ment, main = "Communities Option 3")
+plot(fun4, graph_ment, main = "Communities Option 4")
 dev.off()
 
 ## Create an object 'i' containing the memberships of the fast-greedy community detection
@@ -132,38 +235,44 @@ i <-  membership(fun)
 sizes(fun)
 
 # Add a color attribute to each vertex, setting the vertex color based on community membership
-g_test <- set_vertex_attr(graph_ment, "color", value = c("yellow", "blue", "red", "purple")[i])
+g_test <- set_vertex_attr(graph_ment, "color", value = my_pal[1:4][i])
 
 # Plot the graph using threejs
-graphjs(g_test)
+graphjs(g_test
+        , vertex.label = "name"
+        )
+
 i <- membership(fun)
 
 g_ment <- ggraph(graph_ment, layout = "with_kk") +
   geom_edge_link(color = my_pal[[5]]
                  , alpha = .3) +
-  geom_node_point(aes(color = fun)#my_pal[[5]]
-                  , size = 8) +
+  geom_node_point(
+                   size = 8) +
   geom_node_text(aes(label = name)
-                 , color = "white")+
+                 , color = "white"
+                 , size = 6)+
   theme.graph()
 
+g_ment
 
 
 #Interactive version
 # Set a vertex attribute called 'color' to 'dodgerblue' 
-ment_int <- set_vertex_attr(graph_ment, "color", value = "dodgerblue")
+#ment_int <- set_vertex_attr(graph_ment, "color", value = "dodgerblue")
 
 # Redraw the graph and make the vertex size 1
-graphjs(ment_int, vertex.size = 1)
+#graphjs(ment_int, vertex.size = 1)
 
 # Create numerical vector of vertex eigenvector centralities 
-ec <- as.numeric(eigen_centrality(graph_ment)$vector)
+#ec <- as.numeric(eigen_centrality(graph_ment)$vector)
 
 # Create new vector 'v' that is equal to the square-root of 'ec' multiplied by 5
-v <- 5*sqrt(ec)
+#v <- 5*sqrt(ec)
 
 # Plot threejs plot of graph setting vertex size to v
-graphjs(graph_ment, vertex.size = v)
+#graphjs(graph_ment, vertex.size = v)
+
 
 
 ### Example code
@@ -174,11 +283,6 @@ graphjs(graph_ment, vertex.size = v)
   #geom_edges() + 
   #geom_nodes() + 
   #theme_blank() 
-
-#interactive_network <- static_network + 
- # geom_point_interactive(
-  #  aes(tooltip = name, data_id = R_user)
-  #)
 
 
 # Print the interactive network
@@ -191,23 +295,40 @@ graphjs(graph_ment, vertex.size = v)
   #  opts_tooltip(offx = 10)
   #)
 
-# From previous step
-#nd3 <- igraph_to_networkD3(graph_ment)
+# Make a D3 object
 
-# Render your D3.js network
-#forceNetwork(
-  # Define links from nd3 object
- # Links = nd3$links, 
+
+#add communities to the object
+comm <- membership(fastgreedy.community(graph_ment))
+
+V(graph_ment)$comm <- comm
+V(graph_ment)$betw <- betweenness(graph_ment)
+
+
+
+V(graph_ment)$group <- comm
+nd3 <- igraph_to_networkD3(graph_ment, V(graph_ment)$group)
+
+
+
+#Render your D3.js network
+d3 <- forceNetwork(
+#  Define links from nd3 object
+  Links = nd3$links, 
   # Define nodes from nd3 object
-  #Nodes = nd3$nodes, 
+  Nodes = nd3$nodes, 
   # Specify the source column
-  #Source = "source", 
+  Source = "source", 
   # Specify the target column
-  #Target = "target", 
-  #NodeID = "name",
-  #Group = "group",  
-  #legend = TRUE, 
-  #fontSize = 20
-#)
+  Target = "target", 
+  NodeID = "name",
+  Group = "group",  
+  legend = FALSE, 
+  fontSize = 20,
+  opacity = .8
+  
+)
 
+saveNetwork(d3,
+  "./network_analysis_sandbox/viz/d3-object.html")
 
