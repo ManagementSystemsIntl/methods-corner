80aac52d742e89c36bbc0c838d810d8771f3bee0
Calhoon <BRIAN.CALHOON@tetratech.com>
2023-09-25 17:08:53 -0400

    BC

--- network_analysis_sandbox/scripts/networks-explore-pres.R
+++ network_analysis_sandbox/scripts/networks-explore-pres.R
@@ -8,24 +8,32 @@ set.seed(1374)
 df <- data.frame(name = c("Steve", "Steve", "Steve", "Cheryl", "Stephanie", "Stephanie")
                  , friend = c("Chris", "Cheryl", "Stephanie", "Chris", "Paulo", "Cheryl"))
 
-df <- df |>
-  mutate(team = case_when(name == "Steve" | name == "Chris" ~ "A", name == "Cheryl" | name == "Paulo" ~ "B", name == "Stephanie" ~ "C"))
+df_nodes <- data.frame(name = c("Steve"
+                                , "Cheryl"
+                                , "Stephanie"
+                                , "Chris"
+                                , "Paulo")) 
+df_nodes <- df_nodes |>
+  mutate(team = case_when(name == "Steve" | name == "Chris" ~ "A", name == "Cheryl" | name == "Stephanie" ~ "B", name == "Paulo" ~ "C"))
 
-g <- graph_from_data_frame(df, directed = TRUE)
+g <- graph_from_data_frame(df
+                           , directed = TRUE
+                           , vertices = df_nodes)
 
 
 g_plot <- ggraph(g, layout = "kk") +
-  geom_edge_link(width = 1
+  geom_edge_diagonal(width = 1
                  , color = "grey"
                  , alpha = .7) +
-  geom_node_point(color = "white"
+  geom_node_point(aes(color = team)
                   , size = 16)+
   geom_node_text(aes(label = name)
-                 , color = "#6639B7"
+                 #, color = "#6639B7"
                  , size = 5.5)+ 
   theme_graph(plot_margin = unit(c(1, 0, 1, 0), "cm"))
 
 g_plot
+
 ggsave(plot = g_plot
        , filename  = "friend-plot.png"
        , path = "./network_analysis_sandbox/viz/"
