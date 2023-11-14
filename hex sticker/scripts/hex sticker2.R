4ba319baebf88509f21906f9b43dfb759654f6c8
Calhoon <BRIAN.CALHOON@tetratech.com>
2023-11-14 16:04:53 -0500

    BC

--- hex sticker/scripts/hex sticker2.R
+++ hex sticker/scripts/hex sticker2.R
@@ -11,11 +11,8 @@ library(showtext)
 #read in image
 img <- image_read(here::here("./hex sticker/pics/image.png"))
 
-img
-
-img_crop <- image_crop(img, "500x500+600")
-
-img_crop
+img_text <- image_scale(img, "x200")
+img_text <- image_background(img_text, "#003478", flatten = TRUE)
 
 img_scale <- image_scale(img, "300")
 
@@ -24,20 +21,22 @@ img_scale
 img_crop <- image_crop(img_scale, "175x300+75+10")
 
 img_crop
-img_scale
+
+img_crop <- image_background(img_crop
+                             , "#003478")
 
 ## Loading Google fonts (http://www.google.com/fonts)
 font_add_google("Gochi Hand", "gochi")
 ## Automatically use showtext to render text for future devices
 showtext_auto()
 
-new <- sticker(img_crop, package = "MSI Data Science Team"
+new <- sticker(img_crop, package = ""
                , p_size=20
                , p_color = "#C84E00"
                , s_x=1
-               , s_y=.75
-               , s_width=1
-               , s_height = 1
+               , s_y=1.02
+               , s_width=1.3
+               , s_height = 1.3
                , p_family = "gochi"
                , h_fill = "#003478"
                , h_color = "#C84E00"
